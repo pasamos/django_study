511305c4caa02ba4019c2c0ce744d5f1edb17e62
pasamos <chen_cheng1986@163.com>
2017-02-09 10:34:19 +0800

    add context_processor test

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -64,6 +64,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'djangotest.context_processor.settings',
+                'djangotest.context_processor.ip_address',
             ],
         },
     },
